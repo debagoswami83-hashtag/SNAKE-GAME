e3f927a79a72f69998f16440327000c23ef8fe71
debagoswami83-hashtag <debadritagoswami83@gmail.com>
2026-07-12 00:53:52 +0530

    center game window and update canvas initialization

--- snake.py
+++ snake.py
@@ -13,4 +13,20 @@ window = tkinter.Tk()
 window.title("Snake Game")
 window.resizable(False, False)  
 
+canvas=tkinter.Canvas(window, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, bg="black" , borderwidth=0, highlightthickness=0  )
+canvas.pack()
+window.update()
+
+#centre the window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width / 2) - (window_width / 2))
+window_y = int((screen_height / 2) - (window_height / 2))
+
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+
 window.mainloop()
